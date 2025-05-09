f274b7de769404880dce2b98f511432902067469
jtnugent <nugentj@wlhs.wlwv.k12.or.us>
2025-05-09 09:42:06 -0700

    issues with speed control

--- Project Roguelike/Player_controller.py
+++ Project Roguelike/Player_controller.py
@@ -12,14 +12,16 @@ clock = pg.time.Clock()
 
 
 
-class Player():
+class Player:
     def __init__(self, x, y):
+        velX = 0
+        velY = 0
         self.rect = pg.Rect(x, y, 32, 32)
         self.x = int(x)
         self.y = int(y)
         self.color = (250, 120, 60)
-        self.velX = 0
-        self.velY = 0
+        self.velX = velX
+        self.velY = velY
         self.left_pressed = False
         self.right_pressed = False
         self.up_pressed = False
@@ -31,27 +33,49 @@ class Player():
         pg.draw.rect(win, self.color, self.rect)
 
     def update(self):
+        if self.dash_pressed == False:
+            self.velX = 0
+            self.velY = 0
+            if self.left_pressed and not self.right_pressed:
+                self.velX = -self.speed
+            if self.right_pressed and not self.left_pressed:
+                self.velX = self.speed
+            if self.up_pressed and not self.down_pressed:
+                self.velY = -self.speed
+            if self.down_pressed and not self.up_pressed:
+                self.velY = self.speed
+
+            self.x += self.velX
+            self.y += self.velY
+
+            self.rect = pg.Rect(int(self.x), int(self.y), 32, 32)
+
+    def dash(self):
+        dash_pressed = True
+        self.velX = 0
+        self.dash_pressed = dash_pressed
+        for i in range(50):
+            if last_key_pressed == "left":
+                #Draw
+                win.fill((12, 24, 36))
+                player.draw(win)
+
+                #update
+                player.update()
+                pg.display.flip()
+                clock.tick(120)
+                self.velX = -4
+        self.dash_pressed = False
         self.velX = 0
         self.velY = 0
-        if self.left_pressed and not self.right_pressed:
-            self.velX = -self.speed
-        if self.right_pressed and not self.left_pressed:
-            self.velX = self.speed
-        if self.up_pressed and not self.down_pressed:
-            self.velY = -self.speed
-        if self.down_pressed and not self.up_pressed:
-            self.velY = self.speed
-
-        self.x += self.velX
-        self.y += self.velY
-
-        self.rect = pg.Rect(int(self.x), int(self.y), 32, 32)
 
 
 player = Player(WIDTH/2, HEIGHT/2)
 
 dash_timer = 2
 start_time = 0
+last_key_pressed = ""
+p = Player(0,0)
 
 while True:
 
@@ -62,12 +86,18 @@ while True:
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_LEFT:
                 player.left_pressed = True
+                last_key_pressed = "left"
             if event.key == pg.K_RIGHT:
                 player.right_pressed = True
+                last_key_pressed = "right"
             if event.key == pg.K_UP:
                 player.up_pressed = True
+                last_key_pressed = "up"
             if event.key == pg.K_DOWN:
                 player.down_pressed = True
+                last_key_pressed = "down"
+            if event.key == pg.K_w:
+                p.dash()
 
         if event.type == pg.KEYUP:
             if event.key == pg.K_LEFT:
@@ -78,8 +108,8 @@ while True:
                 player.up_pressed = False
             if event.key == pg.K_DOWN:
                 player.down_pressed = False
-            if event.key == pg.K_w:
-                player.dash_pressed = False
+            
+        
 
     #Draw
     win.fill((12, 24, 36))
